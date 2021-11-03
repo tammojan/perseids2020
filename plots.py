a87642b8bae98ee50f1f64768346f25175bfb683
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2021-11-03 20:27:30 +0100

    Color by entry speed

--- plots.py
+++ plots.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colorbar import ColorbarBase
+import matplotlib.cm as cmx
+import matplotlib.colors as mcolors
+import cmasher
+
 from matplotlib.ticker import ScalarFormatter
 
 from astropy.io import ascii
+
 
 if __name__ == "__main__":
     d = ascii.read("aartfaac_cams_perseids2020.ecsv")
@@ -41,12 +47,26 @@ if __name__ == "__main__":
     plt.savefig("altitudes.png", bbox_inches="tight")
 
 
-    c = d["AARTFAAC_duration"] > 0
-        
 
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(6, 8))
 
-    ax1.plot(d["Int-mV"][c], d["AARTFAAC_duration"][c], "k.")
+    d["Vinf_distance_from_median"] = np.abs(d["Vinf"] - 60)
+    d.sort("Vinf_distance_from_median")
+
+    c = d["AARTFAAC_duration"] > 0
+
+    entryspeed_norm = mcolors.Normalize(vmin=30, vmax=90)
+    # min 20, max 72, but make symmetric around 60
+    print(d["Vinf"][c].min(), d["Vinf"][c].max())
+    entryspeed_colors = cmasher.guppy_r(entryspeed_norm(d["Vinf"][c]))
+    ax1.scatter(d["Int-mV"][c], d["AARTFAAC_duration"][c], color=entryspeed_colors, s=10)
+    cax = fig.add_axes([0.88, 0.72, 0.02, 0.15])
+    scalarMap = cmx.ScalarMappable(norm=entryspeed_norm, cmap=cmasher.guppy_r)
+    scalarMap.set_array([])
+    colorbar = plt.colorbar(scalarMap, cax = cax,orientation='vertical')
+    colorbar.ax.tick_params(labelsize=6)
+    colorbar.set_label("Entry speed (km/h)", size=8)
+
     ax1.set_xlim(2, -11)
     ax1.set_yscale("log")
     ax1.yaxis.set_major_formatter(ScalarFormatter())
